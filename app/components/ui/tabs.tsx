bfe29c5d5594c61d91ee17d3a7cb361273329c1f
George Emmanuel Thomas <georgeemmanuelthomas@gmail.com>
2024-08-27 15:56:42 +0530

    update(Navbar, tabs, favicon) : made navbar responsive, updated the meet the team UI and made it responsive, changed the favicon to csi logo

--- app/components/ui/tabs.tsx
+++ app/components/ui/tabs.tsx
@@ -2,13 +2,14 @@
 "use client";
 
 import { useState } from "react";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { cn } from "@/utils/cn";
 
 type Tab = {
   title: string;
   value: string;
   content?: React.ReactNode;
+  imageUrl?: string; // Assuming tabs might have images
 };
 
 interface TabsProps {
@@ -30,33 +31,51 @@ export const Tabs: React.FC<TabsProps> = ({
 
   return (
     <div className="relative">
+      {/* Tab Buttons */}
       <div
-        className={cn("flex items-center overflow-auto", containerClassName)}
+        className={cn(
+          "flex overflow-x-auto md:overflow-visible whitespace-nowrap md:flex-row items-center",
+          containerClassName
+        )}
       >
         {propTabs.map((tab) => (
           <button
             key={tab.title}
             onClick={() => setActive(tab)}
             className={cn(
-              "px-4 py-2 rounded-full transition-colors duration-300",
+              "px-6 py-3 md:py-2 md:px-4 rounded-lg md:rounded-full transition-colors duration-300 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500",
               tabClassName,
               active.value === tab.value ? activeTabClassName : "text-gray-600"
             )}
           >
+            {tab.imageUrl && (
+              <img
+                src={tab.imageUrl}
+                alt={`${tab.title} image`}
+                className="w-6 h-6 md:w-10 md:h-10 mr-2" // Smaller on mobile, larger on desktop
+              />
+            )}
             {tab.title}
           </button>
         ))}
       </div>
-      <motion.div
-        className={cn("mt-4", contentClassName)}
-        key={active.value}
-        transition={{ type: "spring", bounce: 0.3, duration: 0.6 }}
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: -20 }}
-      >
-        {active.content}
-      </motion.div>
+
+      {/* Tab Content */}
+      <AnimatePresence mode="wait" initial={false}>
+        <motion.div
+          className={cn(
+            "mt-4 rounded-xl p-8 bg-[rgba(17,25,40,0.4)] backdrop-blur-lg backdrop-saturate-150 border border-[rgba(255,255,255,0.125)]",
+            contentClassName
+          )}
+          key={active.value}
+          transition={{ type: "spring", bounce: 0.3, duration: 0.6 }}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -20 }}
+        >
+          {active.content}
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 };
